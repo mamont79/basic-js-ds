fe9d6a1f51a323f5691534ae6e20ece8d9f3a00e
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-04-17 23:01:00 +0200

    feat: add part of binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,23 +1,48 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
+
 class BinarySearchTree {
+
     constructor() {
         this.way = null;
     }
-
 
     root() {
         return this.way;
     }
 
-    add(data) {
 
+    // adding new element, additional recursion
+    add(data) {
+        let newNode = new Node(data);
+
+        if (this.way == null) {
+            this.way = newNode;
+        } else {
+            this.addNewNode(this.way, newNode);
+        }
+    }
+    addNewNode(newPoint, newNode) {
+        if (newNode.data > newPoint.data) {
+            if (newPoint.right == null) {
+                newPoint.right = newNode;
+            } else {
+                this.addNewNode(newPoint.right, newNode);
+            }
+        } else {
+            if (newPoint.left == null) {
+                newPoint.left = newNode;
+            } else {
+                this.addNewNode(newPoint.left, newNode);
+            }
+        }
+        return newPoint;
     }
 
     has(data) {
@@ -33,11 +58,27 @@ class BinarySearchTree {
     }
 
     min() {
-
+        if (!this.way) {
+            return null;
+        } else {
+            let thisPoint = this.way;
+            while (thisPoint.left) {
+                thisPoint = thisPoint.left;
+            }
+            return thisPoint.data;
+        }
     }
 
     max() {
-
+        if (!this.way) {
+            return null;
+        } else {
+            let thisPoint = this.way;
+            while (thisPoint.right) {
+                thisPoint = thisPoint.right;
+            }
+            return thisPoint.data;
+        }
     }
 }
 
